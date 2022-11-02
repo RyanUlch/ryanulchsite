a29e1b43ba928ae2decd282d72dad77f722a86e4
Ryan Ulch <RyanUlchDev@gmail.com>
2022-11-02 13:58:21 -0400

    Added Google Maps Embed

--- src/components/Pages/PageContact/PageContact.tsx
+++ src/components/Pages/PageContact/PageContact.tsx
@@ -15,6 +15,12 @@ const PageContact = (props: unknown) => {
 						<p>I reside in Columbus, Ohio. I can only accept jobs that are either local to the area, or are 100% work-from-home currently</p>
 						<p>This may change in the future</p>
 						<img src='#' alt=''/>
+						<iframe className={classes.map}
+							loading="lazy"
+							allowFullScreen
+							referrerPolicy="no-referrer-when-downgrade"
+							src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA72Tg4Xp4O92XC-9ilIGfgyVlVQpS59_Y&q=43220&zoom=11"
+						/>
 					</Expander>
 				</article>
 				<article id='contact'>
